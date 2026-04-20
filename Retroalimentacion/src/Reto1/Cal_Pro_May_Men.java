18b062df46fbc3304be07b3eeb5552ed55e433b3
Oscar <polielbrayan@gmail.com>
2026-04-19 23:31:24 -0500

    Mejora de codigo, mas dinamico

--- Retroalimentacion/src/Reto1/Cal_Pro_May_Men.java
+++ Retroalimentacion/src/Reto1/Cal_Pro_May_Men.java
@@ -4,40 +4,41 @@ import java.util.Scanner;
 
 public class Cal_Pro_May_Men {
     public static void main(String[] args) {
-        Scanner pou=new Scanner(System.in);
-        int[] numeros=new   int[5];
+        Scanner sc=new Scanner(System.in);
 
-        System.out.println("ingrese los numeros a calcular");
-        for (int i=0; i<5; i++){
-            System.out.println("Numero "+ (i+1) + ": ");
-            numeros[i]=pou.nextInt();
+        System.out.println("¿Cuantos numeros desea calcular?");
+        int n=sc.nextInt();
+        if (n<=0){
+            System.out.println("No puede elejir numeros negativos");
+            return;
         }
 
-
+        int[] numeros= new int[n];
+        System.out.println("Ingrese los numeros: ");
+        for (int i=0; i<numeros.length;i++){
+            System.out.println("Numero "+(i + 1)+":");
+            numeros[i]=sc.nextInt();
+        }
         double suma=0;
         for (int num: numeros){
             suma+=num;
         }
-        double promedio= suma/numeros.length;
+        double promedio=suma/numeros.length;
         System.out.println("El promedio es: "+promedio);
-        int mayor=numeros[0];
-        int menor=numeros[0];
 
-        for (int i=0; i<num eros.length;i++){
-
+        int mayor = numeros[0];
+        int menor = numeros[0];
+        for (int i=0;i<numeros.length;i++){
             if (numeros[i]>mayor){
                 mayor=numeros[i];
-
             }
-            if (numeros[i]<menor) {
+            if (numeros[i]<menor){
                 menor=numeros[i];
-
             }
-
         }
-        System.out.println("---------------------------");
-        System.out.println("El número MAYOR es: " + mayor);
-        System.out.println("El número MENOR es: " + menor);
+        System.out.println("------------------------------------------");
+        System.out.println("El mayor es: "+mayor);
+        System.out.println("El menor es: "+menor);
 
     }
 }
